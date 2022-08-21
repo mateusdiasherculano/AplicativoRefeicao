2f2ed0d6d97bdea78428387bbe3152c77c32654e
mateusdiasherculano <mateusdiasherculano>
2022-08-21 02:32:48 -0300

    ok

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/category_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/utils/app_routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos cozinhar?',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(
+          secondary: Colors.amber,
+        ),
+        fontFamily: 'Raleway',
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              subtitle1: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen()
+      },
+    );
+  }
+}
