c50f104dd0744cf4029e55f9c55fe1d8efa1c186
mateusdiasherculano <mateusdiasherculano>
2022-08-22 10:53:43 -0300

    ok

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:meals/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
+
+  
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)!.settings.arguments as Meal;
@@ -12,8 +14,50 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: const Center(
-        child: Text('Detalhes da refeição'),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
